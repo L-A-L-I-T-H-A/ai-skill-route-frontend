4bcfaf041352e709fed361c2ecc3155ce91331b7
L-A-L-I-T-H-A <akkapolulalitha@gmail.com>
2026-05-28 21:45:26 +0530

    update backend url

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.getElementById('studentForm').addEventListener('submit',async function(
    certifications:Number(certifications.value)
  };
 
- const response=await fetch('https://ai-skill-route-2d2e56.netlify.app/student',{
+ const response=await fetch('https://ai-skill-route-backend-4.onrender.com/student',{
    method:'POST',
    headers:{'Content-Type':'application/json'},
    body:JSON.stringify(data)
